fbc2c232e9d9c9f1dc520adcda435cb49e29ffeb
Claydson Henrique <claydsonhenriquebs@gmail.com>
2024-02-07 14:53:07 -0300

    configurando projeto

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,20 +1,23 @@
 from typing import List, Dict
-import csv  
+import csv
+
 
 class ProcessJobs:
     def __init__(self) -> None:
         self.jobs_list = list()
 
     def read(self, path: str) -> List[Dict]:
-        with open(path,  encoding='utf-8') as readefile:
+        with open(path, encoding="utf-8") as readefile:
             reader = csv.DictReader(readefile)
             self.jobs_list = list(reader)
         return self.jobs_list
 
     def get_unique_job_types(self) -> List[str]:
-        unique_job_types = set(job.get('job_type') for job in self.jobs_list if 'job_type' in job)
+        unique_job_types = set(
+            job.get("job_type") for job in self.jobs_list if "job_type" in job
+        )
         return list(unique_job_types)
-    
+
     def filter_by_multiple_criteria(self, criteria: Dict) -> List[dict]:
         filter_job = []
         for job in self.jobs_list:
@@ -22,13 +25,15 @@ class ProcessJobs:
                 filter_job.append(job)
         return filter_job
 
-if __name__ == "__main__":
-    process_jobs = ProcessJobs()
-    process_jobs.read("src/insights/jobs.py")
 
-    unique_types = process_jobs.get_unique_job_types()
-    print("Tipos únicos de trabalhos:", unique_types)
-    
-    criteria_to_filter = {'job_type': 'Full-time', 'location': 'Remote'}
-    filtered_jobs = process_jobs.filter_by_multiple_criteria(criteria_to_filter)
-    print("Trabalhos filtrados:", filtered_jobs)
+# if __name__ == "__main__":
+#     process_jobs = ProcessJobs()
+#     process_jobs.read("src/insights/jobs.py")
+
+#     unique_types = process_jobs.get_unique_job_types()
+#     print("Tipos únicos de trabalhos:", unique_types)
+
+#     filter = {'job_type': 'Full-time',
+#                           'location': 'Remote'}
+#     filtered_jobs = process_jobs.filter_by_multiple_criteria(filter)
+#     print("Trabalhos filtrados:", filtered_jobs)
--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,9 +1,1 @@
-from src.pre_built.counter import count_ocurrences
-
-
 def test_counter():
-    path = "data/jobs.csv"
-    word = "Python"
-    expected_count= 5
-    result = count_ocurrences(path, word)
-    assert result == expected_count, f"A contagem para a palavra '{word}' no arquivo '{path}' está incorreta. Esperado: {expected_count}, Obtido: {result}"

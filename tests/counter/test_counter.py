66f9fc861a4017913b7c61c8acd1f5bcaba5f510
Claydson Henrique <claydsonhenriquebs@gmail.com>
2024-02-19 22:44:32 -0300

    ajustando requisito 1

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,14 +1,24 @@
-import pytest
 from src.pre_built.counter import count_ocurrences
 
 
-@pytest.mark.xfail
-@pytest.mark.parametrize(
-    "path, word, expected",
-    [
-        ("data/jobs.csv", "Python", 10),
-        ("data/jobs.csv", "Javascript", 5),
-    ],
-)
-def test_counter(path, word, expected):
-    assert count_ocurrences(path, word) == expected
+def test_counter():
+    path = "data/jobs.csv"
+    expected_javascript_count = 122
+    lowercase_javascript = "javascript"
+    upper_case_javascript = "JAVASCRIPT"
+    expected_python_count = 1639
+    lowercase_python = "python"
+    uppercase_python = "PYTHON"
+
+    assert (
+        count_ocurrences(path, lowercase_javascript)
+        == expected_javascript_count
+    )
+
+    assert (
+        count_ocurrences(path, upper_case_javascript)
+        == expected_javascript_count
+    )
+
+    assert count_ocurrences(path, uppercase_python) == expected_python_count
+    assert count_ocurrences(path, lowercase_python) == expected_python_count
